de20dbd043598a36682d31e602c5a1120524dc58
garm-su <github@garm.su>
2023-01-06 00:32:06 +0300

    refactor

--- GridStatus.cs
+++ GridStatus.cs
@@ -15,6 +15,7 @@ using Sandbox.ModAPI.Interfaces;
 using Sandbox.Game.EntityComponents;
 using SpaceEngineers.Game.ModAPI.Ingame;
 using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.GUI.TextPanel;
 
 // Change this namespace for each script you create.
 namespace SpaceEngineers.UWBlockPrograms.GridStatus
@@ -28,6 +29,21 @@ namespace SpaceEngineers.UWBlockPrograms.GridStatus
         List<IMyTerminalBlock> allTBlocks = new List<IMyTerminalBlock>();
         //all armor blocks - be defined
 
+        //cargoLoad,,Таскало 01 TB handleCharged
+        //cargoSave,
+        //Battery,Таскало 01 TB handleCharged
+        //batteryCharge,Recharge
+        //batteryCharge,Auto
+        //ч.Ice: 3000
+        //MyObjectBuilder_Ore.Ice: 42000
+        //MyObjectBuilder_Ore.Gold: 42000
+        //MyObjectBuilder_Ore.Gold: 3000
+
+
+        const String SKIP = "[SKIP]";
+        const double BATTERY_MAX_LOAD = 0.95;
+        Color mainColor = new Color(0, 255, 0);
+
         string statusChannelTag = "RDOStatusChannel";
         string commandChannelTag = "RDOCommandChannel";
 
@@ -37,18 +53,223 @@ namespace SpaceEngineers.UWBlockPrograms.GridStatus
         double damagedBlockRatio = 0;
         double destroyedAmount = 0;
 
-        List<IMyRadioAntenna> antenna;
-        List<MyDetectedEntityInfo> targets = new List<MyDetectedEntityInfo>();
-        Dictionary<string, int> ammoDefaultAmount = new Dictionary<string, int>(); //subtype_id, ammount
-        Dictionary<string, int> itemsDefaultAmount = new Dictionary<string, int>(); //subtype_id, ammount - not ammo
-
         Int32 energyTreshold = 25; //% of max capacity, default - 25%
         Int32 gasTreshold = 25; //% of max capacity, default - 25%
         Int32 uraniumTreshold = 0; //kg
         Int32 damageTreshold = 20; //% of terminal blocks, default - 20%
 
+        List<IMyRadioAntenna> antenna = new List<IMyRadioAntenna>();
+        List<MyDetectedEntityInfo> targets = new List<MyDetectedEntityInfo>();
+        Dictionary<string, int> ammoDefaultAmount = new Dictionary<string, int>(); //subtype_id, ammount
+        Dictionary<string, int> itemsDefaultAmount = new Dictionary<string, int>(); //subtype_id, ammount - not ammo
+
         IMyBroadcastListener commandListener;
         IMyBroadcastListener statusListener;
+
+        public void reReadConfig(Dictionary<string, int> minResourses, String CustomData)
+        {
+            minResourses.Clear();
+
+            foreach (String row in CustomData.Split('\n'))
+            {
+                if (row.Contains(":"))
+                {
+                    var tup = row.Split(':');
+                    if (tup.Length != 2)
+                    {
+                        Echo("Err: " + row);
+                        continue;
+                    }
+                    minResourses[tup[0].Trim()] = Convert.ToInt32(tup[1].Trim());
+                }
+            }
+        }
+
+        private string getName(MyItemType type)
+        {
+            return type.TypeId + '.' + type.SubtypeId;
+        }
+
+        public void cargoLoad(string group, String after)
+        {
+            Echo("cargoLoad: " + group);
+            var blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks, b => b.CubeGrid == Me.CubeGrid && b.HasInventory);
+            var outerCargo = new List<IMyCargoContainer>();
+            var needResources = new Dictionary<string, int>();
+            GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(outerCargo, b => b.CubeGrid != Me.CubeGrid && b.HasInventory);
+
+            bool full = true;
+            double need = 0, found = 0;
+
+            foreach (var block in blocks)
+            {
+                reReadConfig(needResources, block.CustomData);
+                need += needResources.Values.Sum();
+                var items = new List<MyInventoryItem>();
+                for (int j = 0; j < block.InventoryCount; j++)
+                {
+                    block.GetInventory(j).GetItems(items);
+                    foreach (var item in items)
+                    {
+                        var resourceName = getName(item.Type);
+                        if (needResources.ContainsKey(resourceName))
+                        {
+                            needResources[resourceName] -= (int)item.Amount;
+                            found += (int)item.Amount;
+                        }
+                    }
+                }
+
+                var currentfull = moveResources(
+                    outerCargo,
+                    block,
+                    needResources.Where(r => r.Value > 0).ToDictionary(i => i.Key, i => i.Value)
+                );
+                if (!currentfull)
+                {
+                    Echo("NotFull: " + block.CustomName);
+                }
+                full &= currentfull;
+            }
+
+            if (full)
+            {
+                run(after);
+            }
+            else
+            {
+                print("C." + (int)(need == 0 ? 100 : 100 * found / need) + "%");
+            }
+        }
+
+        private bool moveResources(List<IMyCargoContainer> outerCargo, IMyTerminalBlock block, Dictionary<string, int> dictionary)
+        {
+            Echo("Move to " + block.CustomName + " " + dictionary.Keys.Count());
+            if (!block.HasInventory || dictionary.Count == 0) return true;
+            IMyInventory sourse, destination = block.GetInventory();
+            if (destination.IsFull) return true;
+
+            for (int i = 0; i < outerCargo.Count; i++)
+            {
+                if (outerCargo[i].CustomName.Contains(SKIP)) continue;
+
+                for (int j = 0; j < outerCargo[i].InventoryCount; j++)
+                {
+                    var items = new List<MyInventoryItem>();
+                    sourse = outerCargo[i].GetInventory(j);
+                    sourse.GetItems(items);
+                    if (!sourse.IsConnectedTo(destination)) continue;
+                    for (int k = 0; k < items.Count; k++)
+                    {
+                        var item = items[k];
+                        var resourceName = getName(item.Type);
+                        if (dictionary.ContainsKey(resourceName) && dictionary[resourceName] > 0)
+                        {
+                            var countToMove = Math.Min(dictionary[resourceName], (int)item.Amount);
+                            sourse.TransferItemTo(destination, k, null, true);
+                            dictionary[resourceName] -= countToMove;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void cargoSave(string group)
+        {
+            Echo("cargoSave: " + group);
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks, b => b.CubeGrid == Me.CubeGrid && b.HasInventory);
+            var info = new List<String>();
+
+            foreach (var block in blocks)
+            {
+                info.Clear();
+                var items = new List<MyInventoryItem>();
+                for (int j = 0; j < block.InventoryCount; j++)
+                {
+                    block.GetInventory(j).GetItems(items);
+                    foreach (var item in items)
+                    {
+                        info.Add(getName(item.Type) + ": " + item.Amount.ToString());
+                    }
+                }
+                block.CustomData = String.Join("\n", info);
+            }
+        }
+
+        public void print(String s)
+        {
+            var surface = Me.GetSurface(0);
+            surface.Alignment = TextAlignment.CENTER;
+            surface.FontColor = mainColor;
+            surface.FontSize = 4;
+            surface.WriteText(s);
+        }
+
+        public void run(String name)
+        {
+            Echo("Run after: " + name);
+            var tbs = new List<IMyTimerBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyTimerBlock>(tbs, tb => tb.CubeGrid == Me.CubeGrid && tb.CustomName == name);
+
+            Echo("TB found " + tbs.Count);
+            foreach (var tb in tbs)
+            {
+                tb.Trigger();
+            }
+        }
+
+        public void batteryLoad(String after)
+        {
+            Echo("batteryLoad");
+            List<IMyBatteryBlock> batteries = new List<IMyBatteryBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries, b => b.CubeGrid == Me.CubeGrid);
+            float curLoad;
+            if (batteries.Count > 0)
+            {
+                curLoad = 0;
+                foreach (var bat in batteries)
+                {
+                    curLoad += bat.CurrentStoredPower / bat.MaxStoredPower;
+                }
+                curLoad /= batteries.Count;
+
+                print("B." + (int)(100 * curLoad) + "%");
+                if (curLoad > BATTERY_MAX_LOAD)
+                {
+                    run(after);
+                }
+            }
+        }
+
+        public void batteryCharge(string type)
+        {
+            Echo("batteryCharge: " + type);
+            ChargeMode mode;
+            switch (type)
+            {
+                case "Recharge":
+                    mode = ChargeMode.Recharge;
+                    break;
+                case "Discharge":
+                    mode = ChargeMode.Discharge;
+                    break;
+                //.............
+                case "Auto":
+                    mode = ChargeMode.Auto;
+                    break;
+                default:
+                    return;
+            }
+            List<IMyBatteryBlock> batteries = new List<IMyBatteryBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries, b => b.CubeGrid == Me.CubeGrid);
+            foreach (var bat in batteries)
+            {
+                bat.ChargeMode = mode;
+            }
+        }
 
         public Program()
         {
@@ -156,25 +377,22 @@ namespace SpaceEngineers.UWBlockPrograms.GridStatus
                 current_capacity = current_capacity + b.CurrentStoredPower;
                 max_capacity = max_capacity + b.MaxStoredPower;
             }
-            return Math.Round((current_capacity / max_capacity) * 100);
+            return Math.Round((max_capacity == 0 ? 1 : current_capacity / max_capacity) * 100);
         }
 
         public double getCurrentGasAmount(string gasType)
         {
             //gasType {"Oxygen","Hydrogen"}                                            
             List<IMyGasTank> tanks = new List<IMyGasTank>();
-            GridTerminalSystem.GetBlocksOfType<IMyGasTank>(tanks, x => x.CubeGrid == Me.CubeGrid);
+            GridTerminalSystem.GetBlocksOfType<IMyGasTank>(tanks, x => x.CubeGrid == Me.CubeGrid && x.BlockDefinition.SubtypeId.Contains(gasType));
             double current_amount = 0;
             double max_capacity = 0;
             foreach (IMyGasTank t in tanks)
             {
-                if (t.DefinitionDisplayNameText.Contains(gasType))
-                {
-                    current_amount = t.Capacity * t.FilledRatio;
-                    max_capacity = max_capacity + t.Capacity;
-                }
+                current_amount += t.Capacity * t.FilledRatio;
+                max_capacity += t.Capacity;
             }
-            return Math.Round((current_amount / max_capacity) * 100);
+            return Math.Round((max_capacity == 0 ? 1 : current_amount / max_capacity) * 100);
         }
 
         public double getUsedCargoSpace() //% of max
@@ -189,7 +407,7 @@ namespace SpaceEngineers.UWBlockPrograms.GridStatus
                 currentVolume = currentVolume + (double)container.GetInventory(0).CurrentVolume;
             }
 
-            return Math.Round((currentVolume / maxVolume) * 100);
+            return Math.Round((maxVolume == 0 ? 1 : currentVolume / maxVolume) * 100);
         }
 
         public double getGridVelocity()
@@ -248,9 +466,7 @@ namespace SpaceEngineers.UWBlockPrograms.GridStatus
 
         public void Setup()
         {
-            List<IMyTerminalBlock> list = new List<IMyTerminalBlock>();
-            GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(list, x => x.CubeGrid == Me.CubeGrid);
-            antenna = list.ConvertAll(x => (IMyRadioAntenna)x);
+            GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(antenna, x => x.CubeGrid == Me.CubeGrid);
 
             if (antenna.Count() > 0)
             {
@@ -267,8 +483,10 @@ namespace SpaceEngineers.UWBlockPrograms.GridStatus
         {
             if (arg != "")
             {
-                //change parameters
-                if (arg.Contains("="))
+                //change parameters                
+                // .ToList<String>()
+
+                if (arg.Contains("=")) //todo: remove
                 {
                     List<string> args = arg.Split('=').Select(a => a.Trim()).ToList();
                     string cmd = args[0];
@@ -300,26 +518,40 @@ namespace SpaceEngineers.UWBlockPrograms.GridStatus
                     }
                 }
 
-                //set flags
-                if (arg == "locked")
-                {
-                    lockedState = true;
-                    Echo("Grid is locked");
-                }
+                arg += ",,";
+                var props = arg.Split(',');
 
-                if (arg == "unlocked")
+                switch (props[0])
                 {
-                    lockedState = false;
-                    Echo("Manual override lock status");
+                    case "Battery":
+                        batteryLoad(props[1]);
+                        break;
+                    case "cargoSave":
+                        cargoSave(props[1]);
+                        break;
+                    case "cargoLoad":
+                        cargoLoad(props[1], props[2]);
+                        break;
+                    case "batteryCharge":
+                        batteryCharge(props[1]);
+                        break;
+                    case "locked":
+                        lockedState = true;
+                        Echo("Grid is locked");
+                        break;
+                    case "unlocked":
+                        lockedState = false;
+                        Echo("Manual override lock status");
+                        break;
+                    case "fix":
+                        fixGridState();
+                        Echo("Grid state saved");
+                        break;
+                    default:
+                        break;
                 }
-
-                if (arg == "fix")
-                {
-                    fixGridState();
-                    Echo("Grid state saved");
-                }
-
             }
+
             if (!setupcomplete)
             {
                 //If setupcomplete is false, run Setup method.
