7c5dd947cb0bb0d5ed66dd285cdb4d30345d154f
Herman Shinkarenko <herman@garm.su>
2023-01-12 16:15:46 +0300

    small fixes

--- build/build.ps1
+++ build/build.ps1
@@ -5,5 +5,5 @@ $infile = "../" + $file + "/*.cs"
 $outfile = $file + ".igs"
 
 Write-Output $file
-get-content $infile | Where-Object { $_ -notmatch "^using|\/\/@remove$|^[\s]*\/\/" } | out-file $outfile
+(get-content $infile) -replace 'public new ([a-z]+) Log', 'public $1 Log' | Where-Object { $_ -notmatch "^using|\/\/@remove$|^[\s]*\/\/" } | out-file $outfile
 get-content ../Libraries/*.cs | Where-Object { $_ -notmatch "^using|\/\/@remove$|^[\s]*\/\/" } | out-file -append $outfile
