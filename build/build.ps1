8b59d9ed20e81e51556a15f6ec88fb4864757053
Herman Shinkarenko <herman@garm.su>
2023-01-30 02:54:50 +0300

    minificate

--- build/build.ps1
+++ build/build.ps1
@@ -3,7 +3,9 @@
 $file = $args[0]
 $infile = "../" + $file + "/*.cs"
 $outfile = $file + ".igs"
+$minfile = $file + ".min.igs"
 
 Write-Output $file
 (get-content $infile -Encoding UTF8) -replace 'public new ([a-z]+) Log', 'public $1 Log' | Where-Object { $_ -notmatch "^using|\/\/@remove$|^[\s]*\/\/" } | out-file -Encoding UTF8 $outfile
 get-content ../Libraries/*.cs -Encoding UTF8 | Where-Object { $_ -notmatch "^using|\/\/@remove$|^[\s]*\/\/" } | out-file -Encoding UTF8 -append $outfile
+.\min\csminify.exe $outfile > $minfile
